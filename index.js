137b9bacca366b002e42301c70c59245995467dd
Domas Bitvinskas <domas.bitvinskas@me.com>
2018-10-26 13:55:25 +0000

    Use exponential step growth

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import fluid from '@candour/fluid'
 
 export default (n = 1) => {
-  return fluid(n * 16, n * 16 * 1.2)
+  if (n === 0) return 0
+
+  const exponent = n > 1 ? 1 + n / 10 : 1
+  return fluid(n * 14, n * 16 * exponent)
 }
