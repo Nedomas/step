e91bfdaed07e596fdbf46033725c0e6b448f5eac
Domas <domas.bitvinskas@me.com>
2018-11-09 18:01:59 +0000

    Simplify; move some deps to dev

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 import fluid from '@candour/fluid'
 
-export default (n = 1) => {
-  if (n === 0) return 0
-
-  const exponent = n > 1 ? 1 + n / 10 : 1
-  return fluid(n * 14, n * 16 * exponent)
-}
+export default (n = 1) => fluid(n * 14, n * 18)
